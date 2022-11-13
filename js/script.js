204adbcfc48b80ea9ddad404d72ce75803e05691
Eliza Hynek <eliza.hynek@gmail.com>
2022-11-13 11:38:59 +0100

    add article alogorithm

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
 
   console.log(event);
   console.log('clickedElement (with plus): ' + clickedElement);
 
   /* [DONE] remove class 'active' from all article links  */
+  
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
@@ -14,11 +16,13 @@ const titleClickHandler = function(event){
   }
 
   /* [DONE] add class 'active' to the clicked link */
+
   clickedElement.classList.add('active');
 
   console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
@@ -26,9 +30,18 @@ const titleClickHandler = function(event){
   }
   /* get 'href' attribute from the clicked link */
 
+  const articleAttribute = clickedElement.getAttribute('href');
+  
+  console.log(articleAttribute);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleAttribute);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 
 }
   
